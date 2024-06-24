3fb74920722b5e894869e56bfed7e82455a8cf27
Z1ppzy <mietofff@gmail.com>
2024-06-24 13:30:07 +0300

    fix for telegramapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from 'react';
-
-const App: React.FC = () => {
-  const [user, setUser] = useState<WebAppUser | null>(null);
-
-  useEffect(() => {
-    const tg = window.Telegram.WebApp;
-    tg.ready();
-    setUser(tg.initDataUnsafe.user);
-  }, []);
-
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ЭТО МОЕ Telegram App ПРИЛОЖЕНИЕ!!!</h1>
-        {user && <p>Привет, {user.first_name}!</p>}
+    <div>
+      <header>
+        <h1>Welcome to My Telegram App</h1>
       </header>
     </div>
   );
-};
+}
 
 export default App;
