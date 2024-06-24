f64cd4ffd6e1ad009058427aa72ad0ef5235c35d
Z1ppzy <mietofff@gmail.com>
2024-06-24 15:35:16 +0300

    added style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 function App() {
   return (
-    <div>
+    <div className='bg-slate-500 h-screen'>
       <header>
-        <h1>Welcome to My Telegram App</h1>
+        <h1 className='text-2xl text-center'>Welcome to My Telegram App</h1>
       </header>
+      <div className='px-4'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          excepturi ratione saepe magni? Id, ullam? Rem sed natus consequatur
+          consectetur quam recusandae mollitia qui reiciendis ipsam assumenda
+          nulla pariatur maxime iste repellendus ullam, nostrum deleniti minus!
+          Aut, nulla voluptatum fuga nam similique eligendi doloribus saepe.
+          Repudiandae vel, quis aut beatae eveniet, eligendi reprehenderit
+          aperiam nisi voluptas minus debitis suscipit et! Ut aspernatur nobis
+          accusantium esse excepturi, laboriosam dolore voluptate facere?
+          Maiores labore dicta quisquam, commodi, deleniti nobis dolorum
+          consectetur omnis, officiis facere sed necessitatibus eligendi
+          incidunt enim itaque odit at exercitationem distinctio unde ullam
+          nesciunt pariatur voluptas. Maxime, perspiciatis ea.
+        </p>
+      </div>
     </div>
   );
 }
